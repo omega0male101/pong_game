2006ee79ac5a716ec12044f206158e55f9880121
Eden <edenstevens101@gmail.com>
2017-06-16 16:02:47 +0100

    Adding Starting files

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', () => {
+  const targetDiv = document.getElementById('app');
+  ReactDOM.render(<App />, document.getElementById('app'));
+});
+
+
+
